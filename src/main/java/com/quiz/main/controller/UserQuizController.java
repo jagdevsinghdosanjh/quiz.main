cfa21b4d9392c2b0e04da20a13779d0e04927bd1
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2024-08-25 23:26:51 +0530

    ok

--- src/main/java/com/quiz/main/controller/UserQuizController.java
+++ src/main/java/com/quiz/main/controller/UserQuizController.java
@@ -1,18 +1,24 @@
 package com.quiz.main.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.quiz.main.model.Question;
 import com.quiz.main.model.Quiz;
 import com.quiz.main.model.QuizSubmission;
 import com.quiz.main.model.User;
 import com.quiz.main.service.QuizService;
 import com.quiz.main.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-import java.util.Map;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/quizzes")
